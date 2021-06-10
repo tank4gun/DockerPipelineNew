56d3e4a481248c7981d1d80587e9edb913650714
Glebov Daniil <dania.57.v@gmail.com>
2021-06-10 17:57:50 +0300

    Better logs parsing

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from create_app import app
+from datetime import datetime
 from flask import Flask, url_for, request, render_template
 import pandas as pd
 import mleap.sklearn.pipeline
@@ -32,18 +33,20 @@ def new_model_version():
 @app.route("/send_data", methods=["POST"])
 def send_data():
     app.logger.debug("Got request data %s", request.form)
-    if app.config["deploy_version"] is not None:
-        new_regr = LinearRegression().deserialize_from_bundle("../data", "Linear-regression_{}.node".format(app.config['deploy_version']))
-        with open("../data/Linear-regression_{version}.node/Coef_{version}.txt".format(version=app.config['deploy_version']), "r") as coef_file:
-    	    means = np.array(list(map(float, coef_file.readline().split())))
-    	    stds = np.array(list(map(float, coef_file.readline().split())))
-        app.logger.debug("Got mean data {}, std data {}".format(means, stds))
-        input_vector = np.array([float(request.form[name]) for name in input_names])
-        normalized_vector = (input_vector - means) / (stds * math.sqrt(442))
-        prediction = new_regr.predict([normalized_vector])[0][0]
-    else:
-        normalized_vector = input_vector
-        prediction = normalized_vector[0]
+#    if app.config["deploy_version"] is not None:
+ #       new_regr = LinearRegression().deserialize_from_bundle("../data", "Linear-regression_{}.node".format(app.config['deploy_version']))
+        # with open("../data/Linear-regression_{version}.node/Coef_{version}.txt".format(version=app.config['deploy_version']), "r") as coef_file:
+    	  #  means = np.array(list(map(float, coef_file.readline().split())))
+    	  #  stds = np.array(list(map(float, coef_file.readline().split())))
+        #app.logger.debug("Got mean data {}, std data {}".format(means, stds))
+    input_vector = np.array([float(request.form[name]) for name in input_names])
+        #normalized_vector = (input_vector - means) / (stds * math.sqrt(442))
+        #prediction = new_regr.predict([normalized_vector])[0][0]
+  #      normalized_vector = input_vector
+   #     prediction = normalized_vector[0]
+    #else:
+    normalized_vector = input_vector
+    prediction = normalized_vector[0]
     app.logger.debug(
         "Deploy version: {version}, input vector: {input_vector}, prediction: {prediction}".format(
             version=app.config["deploy_version"],
@@ -51,6 +54,13 @@ def send_data():
             prediction=prediction,
         )
     )
-    requests.post("http://td-agent:9880/app_log", json={"version": app.config["deploy_version"], "input_vector": request.form, "prediction": prediction})
+    # requests.post("http://td-agent:9880/app_log", json={"version": app.config["deploy_version"], "input_vector": request.form, "prediction": prediction})
+    requests.post("http://td-agent:9880/app_log", data="{time} Deploy version: {version}, input vector: {input_vector}, prediction: {prediction}".format(
+            time=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+            version=app.config["deploy_version"],
+            input_vector=input_vector.tolist(),
+            prediction=prediction,
+        )
+)
     return render_template("result_screen.html", model_prediction=prediction)
     
